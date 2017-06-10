76d39595e9e27ce6f84d513f870f4216b993f9ca
masahitojp <randomstep@gmail.com>
2017-06-10 14:41:36 +0900

    first code

---
+++ hint.py
@@ -0,0 +1,20 @@
+from typing import Text, NamedTuple
+
+
+class Employee(NamedTuple):
+    id: int
+    name: Text
+
+
+class Manager:
+    def __init__(self, emp: Employee) -> None:
+        self._emp = emp
+
+    def greeting(self) -> Text:
+        return self._emp.name
+
+
+employee: Employee = Employee(name='Guido', id='x')
+m: Manager = Manager(employee)
+
+print(m.greeting())
